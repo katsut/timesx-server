651d13c7a4f13049f84e23644d39a42a93fa88b4
Katsuhide Tsuruta <katsuhide.tsuruta@nulab.com>
2024-02-20 08:53:17 +0900

    implements post tt

--- handler.py
+++ handler.py
@@ -8,45 +8,40 @@ app = APIGatewayRestResolver()
 
 topic_id = os.environ["TOPIC_ID"]
 typetalk_token = os.environ["TYPETALK_TOKEN"]
+system_content = os.environ["SYSTEM_CONTENT"]
 
 
 def post_typetalk_message(message):
-    url = f"https://typetalk.com/api/v1/topics/{topic_id}/messages"
-    headers = {
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Authorization": f"Bearer {typetalk_token}"
-    }
-    payload = {
-        "message": message
-    }
+    print(message)
+    url = f"https://typetalk.com/api/v1/topics/{topic_id}"
+    headers = {"X-TYPETALK-TOKEN": typetalk_token}
 
-    response = requests.post(url, headers=headers, data=payload)
+    response = requests.post(url, headers=headers, data={"message": message})
     response.raise_for_status()
 
     return response.json()
 
 
-@app.post()
+@app.post("/api")
 def receiveText():
     # Extract the text from the API request
 
+    print(app.current_event)
     text = app.current_event.json_body["message"]
-
-    print(text)
 
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
             {
                 "role": "system",
-                "content": "You are a play-by-play person. From a person's soliloquy, you make statements about what that person is doing, like a baseball or horse race play-by-play man. The user will make the comment, so please be sure to give the actual situation of the soliloquy in Japanese in an objective manner.",
+                "content": syntem_content,
             },
             {"role": "user", "content": text},
         ],
     )
     print(completion.choices)
 
-    post_typetalk_message(completion.choices[0].message)
+    post_typetalk_message(completion.choices[0].message.content)
 
     return {"statusCode": 200, "body": completion.choices[0].message}
 
